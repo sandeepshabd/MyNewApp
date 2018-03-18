0a4a35e9edbfbef661e39da7025e3f2a2ed5d5a4
Sandeep Shabd <sandeep.shabd@lochbridge.com>
2018-03-18 11:51:13 -0400

    showing static list

--- app/controller.js
+++ app/controller.js
@@ -2,5 +2,9 @@
 
 angular.module('myNewApp.controller',[])
 .controller('mainController',["$scope",function($scope){
-	
+	$scope.taskList = [
+	{taskname:"Shopping", statusDone:false},
+	{taskname:"Studying", statusDone:false},
+	{taskname:"Booking ticket", statusDone:false}
+	];
 }]);
